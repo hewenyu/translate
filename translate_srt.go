ddd888aa33155aea37ddb006b504a82929f097bc
hewenyu <yuebanlaosiji@outlook.com>
2025-03-02 11:15:05 +0800

    Update translate_srt.go

--- translate_srt.go
+++ translate_srt.go
@@ -1,23 +1,9 @@
 package translate
 
-import "context"
+import (
+	"context"
+)
 
-// translate srt file
-
-type SrtTranslator struct {
-	t Translator // 翻译器
-}
-
-// NewSrtTranslator 创建一个新的 Srt 翻译器
-func NewSrtTranslator(t Translator) *SrtTranslator {
-	return &SrtTranslator{
-		t: t,
-	}
-}
-
-// Translate 翻译字幕文件
-func (t *SrtTranslator) Translate(ctx context.Context, srt string, sourceLang string, targetLang string) (string, error) {
-	// reed srt file
-	
-	return "", nil
+type TranslateSrt interface {
+	Translate(ctx context.Context, srtPath string, sourceLang string, targetLang string) (string, error)
 }
